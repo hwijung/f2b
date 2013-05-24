8d637dfc68381dd97a68726c849b9be8b7c55ff4
Hwijung <hwijung.ryu@gmail.com>
2013-05-24 23:20:19 +0900

    initial commit

---
+++ server-scripts/create_tables.sql
@@ -0,0 +1,27 @@
+CREATE DATABASE f2w;
+use f2w;
+
+CREATE TABLE user
+(
+	user varchar(255) NOT NULL,
+	password varchar(255) NOT NULL,
+	PRIMARY KEY (user)
+);
+
+CREATE TABLE fb_account
+(
+	user varchar(255) NOT NULL,
+	fb_user varchar(32) NOT NULL,
+	fb_access_token varchar(255) NOT NULL,
+	fb_name varchar (32) NOT NULL,
+	PRIMARY KEY (user)
+);
+
+CREATE TABLE wp_account
+(
+	user varchar(255) NOT NULL,
+	wp_address varchar(255) NOT NULL,
+	wp_id varchar(255) NOT NULL,
+	wp_password varchar(255) NOT NULL,
+	PRIMARY KEY (user)
+);
