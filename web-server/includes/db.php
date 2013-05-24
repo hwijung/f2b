0007758c29572e1baa83819f880b54a22fccf8f3
Hwijung <hwijung.ryu@gmail.com>
2013-05-24 23:17:21 +0900

    initial commit

---
+++ web-server/includes/db.php
@@ -0,0 +1,22 @@
+<?php
+	function create_db_connection () {
+		// Create PDO Connection
+		$config['dbconnect'] = array (
+				'host' => 'localhost',
+				'dbname' => 'f2w',
+				'username' => 'root',
+				'password' => '!qazxsw2'
+			);
+
+		try {
+			$dbh = new PDO ( 'mysql:host='.$config['dbconnect']['host'].'; dbname='.$config['dbconnect']['dbname'], $config['dbconnect']['username'], $config['dbconnect']['password'] );
+		} catch ( PDOException $e )	{
+			$e->getMessage ();
+		}
+
+		$dbh->setAttribute( PDO::ATTR_ERRMODE, PDO::ERRMODE_WARNING );
+		$dbh->setAttribute( PDO::ATTR_CASE, PDO::CASE_LOWER );				
+
+		return $dbh;
+	}
+?>
